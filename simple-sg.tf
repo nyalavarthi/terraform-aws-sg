452e9f333285111c677881934203d66dd7997918
Narendra Yalavarthi <nyalavarthi@gmail.com>
2020-01-06 14:11:06 -0500

    Update simple-sg.tf

--- simple-sg.tf
+++ simple-sg.tf
@@ -10,7 +10,7 @@ resource "aws_security_group" "allow_tls" {
     protocol    = "tcp"
     # Please restrict your ingress to only necessary IPs and ports.
     # Opening to 0.0.0.0/0 can lead to security vulnerabilities.
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["10.62.0.0/24"]
   }
 
   tags = {
